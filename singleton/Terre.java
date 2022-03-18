7bf6cd4dad195b8f4dd1d7fa8e89c478e9f8a000
sokayna <raja.laaguel1999@gmail.com>
2022-03-18 22:13:20 +0100

    Initial commit

---
+++ singleton/Terre.java
@@ -0,0 +1,16 @@
+package ma.education.tp4.singleton;
+
+public class Terre {
+	
+	double distanceToSoleil;
+	double  surface;
+	
+	private Terre(double distanceToSoleil, double surface) {
+		this.distanceToSoleil = distanceToSoleil;
+		this.surface = surface;
+		}
+
+	
+	
+
+}
